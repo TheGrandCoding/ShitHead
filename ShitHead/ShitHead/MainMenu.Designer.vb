d920cca6cd8320902b79cd67e323c0fae911946d
jjarhead24 <jarxjared@gmail.com>
2019-11-05 13:44:10 +0000

    t
    t

--- ShitHead/ShitHead/MainMenu.Designer.vb
+++ ShitHead/ShitHead/MainMenu.Designer.vb
@@ -26,6 +26,7 @@ Partial Class MainMenu
         Me.cmdStore = New System.Windows.Forms.Button()
         Me.cmdExit = New System.Windows.Forms.Button()
         Me.cmdLogOut = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'cmdPlay
@@ -34,9 +35,10 @@ Partial Class MainMenu
         Me.cmdPlay.BackgroundImage = Global.ShitHead.My.Resources.Resources.Background
         Me.cmdPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.cmdPlay.ForeColor = System.Drawing.Color.Yellow
-        Me.cmdPlay.Location = New System.Drawing.Point(84, 60)
+        Me.cmdPlay.Location = New System.Drawing.Point(51, 84)
+        Me.cmdPlay.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cmdPlay.Name = "cmdPlay"
-        Me.cmdPlay.Size = New System.Drawing.Size(207, 55)
+        Me.cmdPlay.Size = New System.Drawing.Size(190, 54)
         Me.cmdPlay.TabIndex = 0
         Me.cmdPlay.Text = "Play ShitHead"
         Me.cmdPlay.UseVisualStyleBackColor = False
@@ -46,9 +48,10 @@ Partial Class MainMenu
         Me.cmdStore.BackgroundImage = Global.ShitHead.My.Resources.Resources.Background
         Me.cmdStore.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.cmdStore.ForeColor = System.Drawing.Color.Yellow
-        Me.cmdStore.Location = New System.Drawing.Point(84, 142)
+        Me.cmdStore.Location = New System.Drawing.Point(51, 160)
+        Me.cmdStore.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cmdStore.Name = "cmdStore"
-        Me.cmdStore.Size = New System.Drawing.Size(207, 55)
+        Me.cmdStore.Size = New System.Drawing.Size(190, 54)
         Me.cmdStore.TabIndex = 1
         Me.cmdStore.Text = "Store"
         Me.cmdStore.UseVisualStyleBackColor = True
@@ -58,9 +61,10 @@ Partial Class MainMenu
         Me.cmdExit.BackgroundImage = Global.ShitHead.My.Resources.Resources.Background
         Me.cmdExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.cmdExit.ForeColor = System.Drawing.Color.Yellow
-        Me.cmdExit.Location = New System.Drawing.Point(84, 225)
+        Me.cmdExit.Location = New System.Drawing.Point(10, 239)
+        Me.cmdExit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cmdExit.Name = "cmdExit"
-        Me.cmdExit.Size = New System.Drawing.Size(207, 55)
+        Me.cmdExit.Size = New System.Drawing.Size(135, 32)
         Me.cmdExit.TabIndex = 2
         Me.cmdExit.Text = "Quit"
         Me.cmdExit.UseVisualStyleBackColor = True
@@ -70,28 +74,45 @@ Partial Class MainMenu
         Me.cmdLogOut.BackgroundImage = Global.ShitHead.My.Resources.Resources.Background
         Me.cmdLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.cmdLogOut.ForeColor = System.Drawing.Color.Yellow
-        Me.cmdLogOut.Location = New System.Drawing.Point(84, 304)
+        Me.cmdLogOut.Location = New System.Drawing.Point(149, 239)
+        Me.cmdLogOut.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cmdLogOut.Name = "cmdLogOut"
-        Me.cmdLogOut.Size = New System.Drawing.Size(207, 55)
+        Me.cmdLogOut.Size = New System.Drawing.Size(126, 32)
         Me.cmdLogOut.TabIndex = 3
         Me.cmdLogOut.Text = "Log Out"
         Me.cmdLogOut.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(-2, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(297, 73)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ShitHead"
+        '
         'MainMenu
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.BackgroundImage = Global.ShitHead.My.Resources.Resources.Background
-        Me.ClientSize = New System.Drawing.Size(383, 450)
+        Me.ClientSize = New System.Drawing.Size(287, 290)
         Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.cmdLogOut)
         Me.Controls.Add(Me.cmdExit)
         Me.Controls.Add(Me.cmdStore)
         Me.Controls.Add(Me.cmdPlay)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "MainMenu"
         Me.Text = "Main Menu"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -99,4 +120,5 @@ Partial Class MainMenu
     Friend WithEvents cmdStore As Button
     Friend WithEvents cmdExit As Button
     Friend WithEvents cmdLogOut As Button
+    Friend WithEvents Label1 As Label
 End Class
